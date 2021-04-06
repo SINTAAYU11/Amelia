bf41f0e0a07b0a900fb4f8007314ef89621724b3
SINTAAYU11 <ameliasintaayu21@gamil.com>
2021-04-06 16:41:26 +0700

    Tugas vue.js ( Mari Belajar )

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-fluid">
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Home</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">about</a></li>
+        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Contact us</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Social Media</a></li>
+      </ul>
+    </div>
+    
+    
+    <img alt="Vue logo" src="./assets/img1.jpg">
+    <HelloWorld msg="Mari Belajar"/>
+      
+      <p>Pembelajaran</p>
+     
   </div>
 </template>
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -22,7 +37,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
+}
+
+
+ul {
+  padding-top: 10px;
+  background-color: rgb(120, 221, 221);
+  height: 30px;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color:rgb(0, 0, 0);
 }
 </style>
