37e805a8adbb612b853c71feea0135853db7c7a9
SINTAAYU11 <ameliasintaayu21@gamil.com>
2021-04-08 16:18:12 +0700

    TUGAS mari belajar (4-8-2021)

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
         <b-nav-item active>Home</b-nav-item>
         <b-nav-item active>About</b-nav-item>
         <b-nav-item active>Testi</b-nav-item>
+        <b-nav-item active >Sumber Lainnya</b-nav-item>
         <b-nav-item active >Contact</b-nav-item>
-        <b-nav-item active>Mata. Pembelajaran</b-nav-item>
       </b-nav>
   </div>
     
@@ -14,25 +14,64 @@
       <img alt="Vue logo" src="./assets/img1.jpg">
       <HelloWorld msg="Mari Belajar"/>
     </div>
+
+    <div class="container">
+        <div class="font mt-5" style="font-size:25px; font-weight:bold; text-align: center; font-family:Lucida Calligraphy">About</div>
+    </div>
     
     <div class="container-fluid">
       <div class="row">
         <div class="bg-info mt-3">
           <div class="col mt-3">
-              <img alt="Vue logo" src="./assets/img2.jpeg">
-            </div>
-        <div class="col mt-3"  style="color: white;">
-         "Mari belajar" yang menyediakan bahan belajar serta fasilitas komunikasi yang mendukung interaksi antar komunitas. 
-         Mari Belajar hadir sebagai pengganti belajar tatap muka yang dapat dimanfaatkan oleh siswa dan guru Pendidikan Anak Usia Dini (PAUD),
-         Sekolah Dasar (SD), Sekolah Menengah Pertama (SMP), Sekolah Menengah Atas/Kejuruan (SMA/SMK) sederajat. Dengan menggunakan Mari Belajar, 
-         kita dapat belajar di mana saja, kapan saja dengan siapa saja. Seluruh konten yang ada di Rumah Belajar dapat diakses dan dimanfaatkan secara gratis.
+              <img alt="Vue logo" src="./assets/img3.jpeg">
+          </div>
+          <div class="col mt-3" style="color: white;">
+          "Mari belajar" yang menyediakan bahan belajar serta fasilitas komunikasi yang mendukung interaksi antar komunitas. 
+          Mari Belajar hadir sebagai pengganti belajar tatap muka yang dapat dimanfaatkan oleh siswa dan guru Pendidikan Anak Usia Dini (PAUD),
+          Sekolah Dasar (SD), Sekolah Menengah Pertama (SMP), Sekolah Menengah Atas/Kejuruan (SMA/SMK) sederajat. Dengan menggunakan Mari Belajar, 
+          kita dapat belajar di mana saja, kapan saja dengan siapa saja. Seluruh konten yang ada di Rumah Belajar dapat diakses dan dimanfaatkan secara gratis.
+          </div>
         </div>
-            
-        </div>
+
+      <div class="container">
+        <div class="font mt-5" style="font-size:25px; font-weight:bold; text-align: center;  font-family:Arial Black">Testi</div>
       </div>
-        <div class="container">
-        <div class="row">
-           <div class="col mt-5">
+        
+      <div class="col-6 mt-5">
+          <img alt="Vue logo" src="./assets/imgd.jpg">
+        </div>
+        <div class="col-6 mt-5">
+          <div class="font mt-2" style="font-weight:bold;">
+            <p>Bu Mardiana (Guru SMK)</p>
+          Saya suka bermain AR Rumah Belajar, sangat menarik, jaman now banget, apalagi yang bagian -bagian sel. Luar biasa. Sangat jelas,
+          dan benad -benar berasa ada di dekat saya. Gemes pengen saya sentuh sel nya.
+          </div>
+        </div>
+        
+
+        <div class="col-6 mt-5">
+          <img alt="Vue logo" src="./assets/imgc.png">
+        </div>
+        <div class="col-6 mt-5">
+          <div class="font mt-3" style="font-weight:bold;">
+            <p>Bu Ismi (Guru SMA)</p>
+          Lewat HP, saya bisa memanfaatkan Rumah Belajar untuk mengerjakan tugas-tugas dari guru,
+           semua menjadi mudah, karena HP tidak hanya untuk telepon dan kirim pesan, tapi bisa juga untuk belajar.
+          </div>
+        </div>
+
+
+        <div class="col-6 mt-5">
+          <img alt="Vue logo" src="./assets/imgb.jpg">
+        </div>
+        <div class="col-6 mt-5">
+          <div class="font mt-3" style="font-weight: bold;">
+            <p>Bu Dian (Guru SMP)</p>
+           Cara mudah & murah untuk memulai pembelajaran yg tak kenal waktu, tempat & usia.
+            Gunakan cara ini, demi keunggulan Anak Didik Bangsa.
+          </div>
+        </div>
+        <div class="col-6 mt-5">
               <img alt="Vue logo" src="./assets/imga.jpg">
             </div>
         <div class="col mt-5">
@@ -43,43 +82,75 @@
             </div>
         </div>
 
-        <div class="col mt-5">
-          <img alt="Vue logo" src="./assets/imgb.jpg">
+      </div>
+      
+  
+        <div class="container">
+        <div class="row">
+        <div class="container">
+          <div class="font mt-5" style="font-size:25px; font-weight:bold; text-align: center; font-family: Lucida Calligraphy;">Sumber Belajar Lainya</div>
         </div>
+
+          <div class="container">
+          <div class="row">
+             <div class="col align-self-start mt-5">
+              <img alt="logo" src="./assets/img4.jpeg">
+             </div>
+            <div class="col align-self-center mt-5">
+              <img alt="logo" src="./assets/img6.png"> 
+            </div>
+            <div class="col align-self-end mt-5">
+              <img alt="logo" src="./assets/img8.jpg"> 
+            </div>
+            <div class="col-6 align-self-end mt-5">
+              <img alt="logo" src="./assets/img7.jpg"> 
+            </div>
+            <div class="col-6 align-self-end mt-5">
+               <img alt="logo" src="./assets/img5.png"> 
+            </div>
+         </div>
+          <div class="container">
+          <div class="font mt-5" style="font-size:25px; font-weight:bold; text-align: center; font-family: Lucida Calligraphy;">Contact</div>
+        </div>
+        </div>
+
+        <div class="container-fluid">
+        <footer class="text-center text-white" style="background-color: #f1f1f1;">
+            <div class="container pt-4">
+                <section class="mb-4"></section>
+                <a
+                    class="btn btn-primary btn-floating m-1"
+                    style="background-color: #3b5998;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-facebook-f"></i
+                 ></a>
+
+                 <a
+                    class="btn btn-primary btn-floating m-1"
+                    style="background-color: #55acee;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-twitter"></i
+                ></a>
+
+                <a
+                    class="btn btn-primary btn-floating m-1"
+                    style="background-color: #ac2bac;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-instagram"></i
+                ></a>
+            </div>
+            <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+              © 2020 Copyright:
+              <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+       </footer>
         
-        <div class="col mt-5">
-          <div class="font mt-3" style="font-weight: bold;">
-            <p>Bu Dian (Guru SMP)</p>
-           Cara mudah & murah untuk memulai pembelajaran yg tak kenal waktu, tempat & usia.
-            Gunakan cara ini, demi keunggulan Anak Didik Bangsa.
-          </div>
-        </div>
-        
-        <div class="col mt-5">
-          <img alt="Vue logo" src="./assets/imgc.png">
-        </div>
-        
-        <div class="col mt-5">
-          <div class="font mt-3" style="font-weight:bold;">
-            <p>Bu Ismi (Guru SMA)</p>
-          Lewat HP, saya bisa memanfaatkan Rumah Belajar untuk mengerjakan tugas-tugas dari guru,
-           semua menjadi mudah, karena HP tidak hanya untuk telepon dan kirim pesan, tapi bisa juga untuk belajar.
-          </div>
-        </div>
-       
-        <div class="col mt-5">
-          <img alt="Vue logo" src="./assets/imgd.jpg">
-        </div>
-        
-        <div class="col mt-5">
-          <div class="font mt-2" style="font-weight:bold;">
-            <p>Bu Mardiana (Guru SMK)</p>
-          Saya suka bermain AR Rumah Belajar, sangat menarik, jaman now banget, apalagi yang bagian -bagian sel. Luar biasa. Sangat jelas,
-          dan benad -benar berasa ada di dekat saya. Gemes pengen saya sentuh sel nya.
-          </div>
         </div>
         </div>
-        </div>
+      </div>
     </div>
   </div>
     
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,6 +25,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1{
+  font-weight: bold;
+}
 h3 {
   margin: 40px 0 0;
 }
